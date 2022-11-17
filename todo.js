4ce654d765551bea94b26c129df2d0c6a09b2248
Dharavath Supriya <dharavathsupriya510@gmail.com>
2022-11-18 00:38:13 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,30 @@
+const todoList = () => {
+    let all = [];
+    const add = (todoItem) => {
+      all.push(todoItem)
+    }
+    const markAsComplete = (index) => {
+      all[index].completed = true
+    }
+  //yesterday
+    const overdue = () => {
+      return all.filter((todolist) => todolist.dueDate < new Date().toLocaleDateString("en-CA")
+      );
+    }
+  //today
+    const dueToday = () => {
+      return all.filter((todolist) => todolist.dueDate === new Date().toLocaleDateString("en-CA")
+      );
+    }
+  //tomorrow
+    const dueLater = () => {
+      return all.filter((todolist) => todolist.dueDate > new Date().toLocaleDateString("en-CA")
+      );
+    }
+  
+    function toDisplayableList(list) {
+      return list.map(todolist => `${todolist.completed ? `[x]` : `[ ]`} ${todolist.title} ${todolist.dueDate == today ?" ": todolist.dueDate}`).join('\n')
+    }
+    return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
+  }
+  module.exports = todoList;
